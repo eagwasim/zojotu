87b880502fa3bca469f7832775cec28f99846f0e
Emmanuel Agwasim <agwasim.e@gmail.com>
2026-07-11 02:55:23 +0200

    updated maingun sending

--- src/lib/email/mailgun.ts
+++ src/lib/email/mailgun.ts
@@ -1,4 +1,6 @@
 import type { EmailProvider, EmailMessage } from "./provider";
+import FormData from "form-data"; // form-data v4.0.1
+import Mailgun from "mailgun.js"; // mailgun.js v11.1.0
 
 export class MailgunProvider implements EmailProvider {
   private apiKey: string;
@@ -12,24 +14,44 @@ export class MailgunProvider implements EmailProvider {
   }
 
   async send(message: EmailMessage) {
-    const url = `https://api.mailgun.net/v3/${this.domain}/messages`;
-
-    const form = new URLSearchParams();
-    form.append("from", this.from);
-    form.append("to", message.to);
-    form.append("subject", message.subject);
-    form.append("html", message.html);
-    if (message.text) form.append("text", message.text);
-
-    console.log(message);
-
-    const res = await fetch(url, {
-      method: "POST",
-      headers: {
-        Authorization: `Basic ${Buffer.from(`api:${this.apiKey}`).toString("base64")}`,
-      },
-      body: form,
+    const mailgun = new Mailgun(FormData);
+    const mg = mailgun.client({
+        username: "api",
+        key: this.apiKey,
+        url: "https://api.eu.mailgun.net"
     });
+
+    try {
+        const data = await mg.messages.create("mg.zojotu.com", {
+          from: this.from,
+          to: [message.to],
+          subject: message.subject,
+          html: message.html || message.text,
+        });
+
+        return { success: true, messageId: data.id };
+    } catch (error) {
+        console.error("[Mailgun] Send failed:", error);
+        return { success: false };
+    }
+ //   const url = `https://api.eu.mailgun.net/v3/${this.domain}/messages`;
+
+   // const form = new URLSearchParams();
+   // form.append("from", this.from);
+   // form.append("to", message.to);
+   // form.append("subject", message.subject);
+   // form.append("html", message.html);
+  //  if (message.text) form.append("text", message.text);
+
+   // console.log(message);
+
+   // const res = await fetch(url, {
+   //   method: "POST",
+   //   headers: {
+   //     Authorization: `Basic ${Buffer.from(`api:${this.apiKey}`).toString("base64")}`,
+    //  },
+    //  body: form,
+    //});
 
     if (!res.ok) {
       const err = await res.text();
